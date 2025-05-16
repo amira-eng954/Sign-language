2581ad33567e27d0d6d23875863b1242787fd7f9
amira-eng954 <106436705+AmiraAli123@users.noreply.github.com>
2025-05-16 22:50:10 +0300

    update12

--- app/Http/Controllers/Api/DictionaryController.php
+++ app/Http/Controllers/Api/DictionaryController.php
@@ -15,7 +15,7 @@ class DictionaryController extends Controller
         $dictionary=$dictionary->map(function($item)
 
         {
-            $item->media_path="https://sign-anguage-dmf5fvf9d6ffg9b6.scm.uaenorth-01.azurewebsites.net/wwwroot/public/{$item->media_path}";
+            $item->media_path="https://signlanguage.blob.core.windows.net/upload/{$item->media_path}";
             return $item;
 
         });
@@ -29,7 +29,7 @@ class DictionaryController extends Controller
         $one=Dictionary::find($id);
       
         if($one){
-           $one->media_path = "https://sign-anguage-dmf5fvf9d6ffg9b6.scm.uaenorth-01.azurewebsites.net/wwwroot/public/{$one->media_path}";
+           $one->media_path = "https://signlanguage.blob.core.windows.net/upload/{$one->media_path}";
            
         return response()->json([
            
